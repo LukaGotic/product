ca2435fab0a434397aa1f2fdd1f5e9ade6d27de3
LukaGotic <gotic.luka@gmail.com>
2025-03-02 20:23:25 +0100

    Setting up environment for easier project startup

---
+++ src/main/resources/db/migration/V1_0_0__init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    code CHAR(10) NOT NULL UNIQUE CHECK (LENGTH(code) = 10),
+    name VARCHAR(255) NOT NULL,
+    price_eur NUMERIC(10,2) NOT NULL CHECK (price_eur >= 0),
+    price_usd NUMERIC(10,2) NOT NULL CHECK (price_usd >= 0),
+    is_available BOOLEAN DEFAULT TRUE
+);
+
+CREATE INDEX idx_product_code ON product (code);
